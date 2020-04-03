b43e4af0c6b1fe2eb84acff4fd59ac02cf85ae04
Freek Heining <freek.heining@gmail.com>
2020-04-03 13:04:31 +0200

    Working 1, 2, + and =

--- Rekenmachine_v1/MainWindow.xaml.cs
+++ Rekenmachine_v1/MainWindow.xaml.cs
@@ -25,18 +25,22 @@ namespace Rekenmachine_v1
             InitializeComponent();
         }
 
-
+        // Invoer opslag variabele
         StringBuilder enteredValue = new StringBuilder();
-        int storedValue = 0;
+
+        // Opslag voor berekeningen
+        double storedValue = 0;
+
+        // Resultaat van laatste berekening
+        double endResult = 0;
  
         private void button_1_Click(object sender, RoutedEventArgs e)
         {
-
+            // Sla tag 'invoer' op in stringbuilder
             enteredValue.Append(button_1.Tag);
             Console.WriteLine("enteredValue: " + enteredValue);
 
-            // StoreValue(enteredValue);
-
+            // show on display
             screenLabel.Content = null + enteredValue;
 
         }
@@ -46,8 +50,6 @@ namespace Rekenmachine_v1
 
             enteredValue.Append(button_2.Tag);
             Console.WriteLine("enteredValue: " + enteredValue);
-
-            // StoreValue(enteredValue);
 
             screenLabel.Content = null + enteredValue;
 
@@ -90,8 +92,21 @@ namespace Rekenmachine_v1
 
         private void button_plus_Click(object sender, RoutedEventArgs e)
         {
-            storedValue = int.Parse(enteredValue.ToString());
-            Console.WriteLine("storedValue: " + storedValue);
+            if (enteredValue.Length > 0)
+            {
+                // Sla huidige stringbuilder invoer op als double
+                storedValue += double.Parse(enteredValue.ToString());
+                Console.WriteLine("storedValue: " + storedValue);
+
+                // Leeg huidige invoer variabele en wacht op nieuwe
+                enteredValue.Clear();
+            }
+            else
+            {
+                // do nothing
+            }
+
+
         }
 
         private void button_min_Click(object sender, RoutedEventArgs e)
@@ -109,10 +124,20 @@ namespace Rekenmachine_v1
 
         }
 
-        private void Button_result_Click(object sender, RoutedEventArgs e)
+        private void button_result_Click(object sender, RoutedEventArgs e)
         {
+            endResult = storedValue + double.Parse(enteredValue.ToString());
+            Console.WriteLine("endResult: " + endResult);
 
+            // Leeg invoer
+            enteredValue.Clear().Append(0);
+
+            // Sla op in geheugen voor verdere berekening
+            storedValue = endResult;
+            // laat zien op display
+            screenLabel.Content = endResult;
         }
+
 
         private void StoreValue(StringBuilder passedValue)
         {
